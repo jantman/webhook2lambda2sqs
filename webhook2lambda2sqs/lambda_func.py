be69ac9be000041d1941613c39ce40303af14b56
Jason Antman <jason@jasonantman.com>
2016-07-24 09:29:37 -0400

    add docs and comments, and better logging and error handling

--- webhook2lambda2sqs/lambda_func.py
+++ webhook2lambda2sqs/lambda_func.py
@@ -32,7 +32,22 @@ endpoints = {}
 
 
 def webhook2lambda2sqs_handler(event, context):
-    # be sure we log full information about any error
+    """
+    Main entry point/handler for the lambda function. Wraps
+    :py:func:`~.handle_event` to ensure that we log detailed information if it
+    raises an exception.
+
+    :param event: Lambda event that triggered the handler
+    :type event: dict
+    :param context: Lambda function context - see
+      http://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    :return: JSON-serialized success response
+    :rtype: str
+    :raises: Exception
+    """
+    # be sure we log full information about any error; if handle_event()
+    # raises an exception, log a bunch of information at error level and then
+    # re-raise the Exception
     try:
         res = handle_event(event, context)
     except Exception as ex:
@@ -40,26 +55,57 @@ def webhook2lambda2sqs_handler(event, context):
         logger.error('Error handling event; event=%s context=%s',
                      event, vars(context), exc_info=1)
         raise ex
+    logger.debug('handle_event() result: %s', res)
+    # if all enqueues failed, this should be an error
     if len(res['SQSMessageIds']) < 1:
-        raise Exception('Failed enqueueing all messages.')
+        raise Exception('Failed enqueueing all messages')
+    # if success, return the success JSON response
     return res
 
 
 def handle_event(event, context):
+    """
+    Do the actual event handling - try to enqueue the request.
+
+    :param event: Lambda event that triggered the handler
+    :type event: dict
+    :param context: Lambda function context - see
+      http://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    :return: JSON-serialized success response
+    :rtype: str
+    :raises: Exception
+    """
+    # endpoint config that's templated in by webhook2lambda2sqs
     global endpoints
     logger.debug('Endpoint Config: %s; Event: %s; Context: %s',
                  pformat(endpoints), pformat(event), pformat(vars(context)))
+    # translate our endpoint path to the endpoints config key
     ep_name = event['context']['resource-path'].lstrip('/')
+    # raise an exception if somehow we got called for an un-configured endpoint,
+    # i.e. if an endpoint was added to the API Gateway manually
     if ep_name not in endpoints:
         raise Exception('Endpoint not in configuration: /%s' % ep_name)
+    # get endpoint config
     ep_conf = endpoints[ep_name]
+    # store some state
     msg_ids = []
     failed = 0
+    # connect to SQS API
     conn = boto3.client('sqs')
-    msg = json.dumps({
+    # find the actual input data - this differs between GET and POST
+    http_method = event.get('context', {}).get('http-method', None)
+    if http_method == 'GET':
+        data = event.get('params', {}).get('querystring', {})
+    else:  # POST
+        data = event.get('body-json', {})
+    # build the message to enqueue
+    msg_dict = {
+        'data': serializable_dict(data),
         'event': serializable_dict(event),
         'context': serializable_dict(vars(context))
-    })
+    }
+    msg = json.dumps(msg_dict)
+    logger.debug('Message to enqueue: %s', msg)
     for queue_name in ep_conf['queues']:
         try:
             logger.debug('Getting Queue URL for queue %s', queue_name)
@@ -75,13 +121,15 @@ def handle_event(event, context):
             msg_ids.append(resp['MessageId'])
         except Exception:
             failed += 1
-            logger.error('Failed enqueueing message in %s: %s', queue_name,
-                         msg, exc_info=1)
+            logger.error('Failed enqueueing message in %s:', queue_name,
+                         exc_info=1)
     fail_str = ''
+    status = 'success'
     if failed > 0:
         fail_str = '; %d failed' % failed
+        status = 'partial'
     return {
-        'status': 'success',
+        'status': status,
         'message': 'enqueued %s messages%s' % (len(msg_ids), fail_str),
         'SQSMessageIds': msg_ids
     }
